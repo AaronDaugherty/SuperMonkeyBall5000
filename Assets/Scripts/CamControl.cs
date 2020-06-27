fba26a19eb667eb6726c1acdf1fefa42c079cc60
Adam Steinberg <adam.l.steinberg@me.com>
2020-06-27 02:31:22 -0400

    mouse controlled camera kinda

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -1,41 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TreeEditor;
 using UnityEngine;
+using UnityEngine.U2D;
 
-public class CamControl : MonoBehaviour { 
+public class CamControl : MonoBehaviour
+{
 
+    public float radius = 10;
     public GameObject player;
-    public float desiredDistance = 10;
-
-    public float dragSpeed = 2;
-    private Vector3 dragOrigin;
+    public float angle = 30.0f;
+    public float stepSize = 0.1f;
+    public float panSpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        desiredDistance += Input.GetAxis("Mouse ScrollWheel");
+        radius -= Input.GetAxis("Mouse ScrollWheel");
+        float horizOffset = radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        float vertOffset = radius * Mathf.Sin(angle * Mathf.Deg2Rad);
 
-        Vector3 playerPos = player.transform.position;
-        playerPos.y = 0;
-        Vector3 cameraPos = transform.position;
-        cameraPos.y = 0;
-        float curr_distance = (playerPos - cameraPos).magnitude;
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            float h = Input.GetAxis("Mouse X");
+            float v = Input.GetAxis("Mouse Y");
+            //limits to keep from panning too far up or down
+            if (transform.up.y <= 0.1 && transform.position.y > player.transform.position.y)
+                v = Mathf.Min(0, v);
+            else if (transform.up.y <= 0.1 && transform.position.y < player.transform.position.y)
+                v = Mathf.Max(0, v);
 
-        Vector3 delta = 0.1f * (curr_distance - desiredDistance) * (playerPos - cameraPos);
-        delta.y = 0;
-        transform.LookAt(player.transform.position);
-
-            transform.position += delta;
+            transform.position += (transform.up * v  + transform.right * h) * panSpeed;
         }
 
-       
+        Vector3 playerPos = player.transform.position + Vector3.up * vertOffset; //this is a point a little bit above the player
 
+        Vector3 diff = playerPos - transform.position; //difference between player and camera positions
+        transform.position += stepSize * (diff.magnitude - horizOffset) * diff;
+        transform.LookAt(player.transform.position);
     }
+}
 
